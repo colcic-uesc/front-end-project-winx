e2254cfaeee78e6bdbf762581e16e6b8615cb888
Everaldina G. Barbosa <everaldinag@gmail.com>
2024-06-23 15:41:24 -0300

    att router file

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,10 @@ const router = createRouter({
   routes: [
     {
       path: '/',
+      redirect: { name: 'home' }
+    },
+    {
+      path: '/inicio',
       name: 'home',
       component: HomeView
     },
