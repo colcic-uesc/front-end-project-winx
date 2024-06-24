9232e2c503f24eaa729afbcf45c88fb4a0a474a2
Ana Cristina P. Morais <acpmorais.cic@uesc.br>
2024-06-24 03:17:36 -0300

    add inputComponent

--- src/components/InputComponent.vue
+++ src/components/InputComponent.vue
@@ -1,8 +1,36 @@
-<script setup>
-</script>
-
 <template>
+    <div class="input-wrap">
+        <label v-if="label">{{ label }}</label>
+        <input 
+            type="text"
+            :value="modelValue"
+            @input="$emit('update:modelValue', $event.target.value)"
+            v-bind="$attrs"
+        />
+    </div>
 </template>
 
-<style scoped>
+<script setup>
+defineProps({
+    label: {
+        type: [String, Boolean],
+        default: false,
+    },
+    modelValue: {
+        type: String,
+        default: "",
+    },
+});
+</script>
+
+<style lang="scss" scoped>
+.input-wrap {
+    display: flex;
+    flex-direction: column;
+
+    input {
+        padding: 8px 12px;
+        font-size: 16px;
+    }
+}
 </style>
