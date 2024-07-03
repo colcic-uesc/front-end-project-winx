04e5514402d2bc09bee8eb64179a97405660bac9
Everaldina G. Barbosa <everaldinag@gmail.com>
2024-07-03 18:19:25 -0300

    feat: studentCardComponent

---
+++ src/components/StudentCardComponent.vue
@@ -0,0 +1,68 @@
+<script setup>
+    import { ref } from 'vue'
+
+    const student = ref({
+        name: 'Nome de um aluno',
+        email: 'aluno@email.com',
+        course: 'Curso de um aluno',
+        CRAA: 9.0,
+        status: 'Cursando'
+    })
+
+    const infoItens = ref([
+        { icon: 'string', text: 'string' },
+        { icon: 'string', text: 'string' },
+    ])
+</script>
+
+
+<template>
+    <div class="student-card">
+        <div class="student-info">
+            <div class="info-item heading-secondary">
+                <font-awesome-icon icon="user" class="icon" />
+                <p class="name">{{ student.name }}</p>
+            </div>
+            <div class="info-item heading-tertiary">
+                <font-awesome-icon icon="envelope" class="icon" />
+                <p class="email">{{ student.email }}</p>
+            </div>
+        </div>
+        <div class="student-details body-text">
+            <p>Curso: {{ student.course }}</p>
+            <p>CRAA: {{ student.CRAA }}</p>
+            <p>Status: {{ student.status }}</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .student-card{
+        display: flex;
+        flex-wrap: wrap;
+        background-color: #f0f0f0;
+        padding: 2rem 7rem;
+        justify-content: space-between;
+        width: 100%;
+        box-sizing: border-box;
+        border-radius: 8px;
+    }
+
+    .student-details{
+        color: var(--color-variant-background);
+    }
+
+    .info-item{
+        display: flex;
+        align-items: center;
+        gap: 0.5rem;
+        color: var(--color-variant-background);
+    }
+
+    p{
+        margin: 0;
+        margin-bottom: 10px;
+    }
+
+</style>
+
